2b1a6334b3f37ecf409af57f6c4aca514f4f8980
JJERY <dev.seojjery@gmail.com>
2024-11-18 01:02:01 +0900

    🎉 Start : Initial Setting

---
+++ src/main/java/com/danpoong/withu/WithUApplication.java
@@ -0,0 +1,13 @@
+package com.danpoong.withu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WithUApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WithUApplication.class, args);
+    }
+
+}
