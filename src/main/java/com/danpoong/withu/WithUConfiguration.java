fa3687cc50c9405116329ecd674429d38179bc0e
JJERY <dev.seojjery@gmail.com>
2024-11-18 02:37:45 +0900

    🔧 Settings: config 추가

---
+++ src/main/java/com/danpoong/withu/WithUConfiguration.java
@@ -0,0 +1,22 @@
+package com.danpoong.withu;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@OpenAPIDefinition(servers = {@Server(url = "/",
+        description = "Default Server URL")})
+
+@Configuration
+public class WithUConfiguration implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods("GET", "POST", "PATCH", "DELETE", "PUT")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
