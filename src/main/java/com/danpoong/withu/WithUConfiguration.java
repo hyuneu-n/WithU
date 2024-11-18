53ab9e8f77676599cc69f7f17ad682b67d3eaeb9
Suhun0331 <rlatngns6046@naver.com>
2024-11-18 12:26:11 +0900

    :wrench: Setting : Add healthcheck and config link

--- src/main/java/com/danpoong/withu/WithUConfiguration.java
+++ src/main/java/com/danpoong/withu/WithUConfiguration.java
@@ -14,7 +14,8 @@ public class WithUConfiguration implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("http://localhost:3000",
+                        "http://ec2-13-209-177-17.ap-northeast-2.compute.amazonaws.com:8080")
                 .allowedMethods("GET", "POST", "PATCH", "DELETE", "PUT")
                 .allowedHeaders("*")
                 .allowCredentials(true);
