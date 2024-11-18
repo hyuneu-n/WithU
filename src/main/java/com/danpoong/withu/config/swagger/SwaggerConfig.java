45749b742c46d8f9fc204f00d7507e78263aa4ee
Suhun0331 <rlatngns6046@naver.com>
2024-11-18 22:29:45 +0900

    :bug: Fix : swaggerConfig ver1

--- src/main/java/com/danpoong/withu/config/swagger/SwaggerConfig.java
+++ src/main/java/com/danpoong/withu/config/swagger/SwaggerConfig.java
@@ -17,40 +17,17 @@ import io.swagger.v3.oas.models.servers.Server;
 
 @Configuration
 public class SwaggerConfig {
-    @Value("${server.servlet.context-path:}")
-    private String contextPath;
+    @Value("${api.server.url}")
+    private String serverUrl;
 
     @Bean
-    public OpenAPI customOpenAPI() {
-        Server localServer = new Server();
-        localServer.setUrl(contextPath);
-        localServer.setDescription("Local Server");
-
-        Server prodServer = new Server();
-        prodServer.setDescription("Production Server");
-
+    public OpenAPI openAPI() {
         return new OpenAPI()
-                .addServersItem(localServer)
-                .addServersItem(prodServer)
-                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
-                .components(
-                        new Components()
-                                .addSecuritySchemes(
-                                        "bearerAuth",
-                                        new SecurityScheme()
-                                                .type(SecurityScheme.Type.HTTP)
-                                                .scheme("bearer")
-                                                .bearerFormat("JWT")))
-                .info(
-                        new Info()
-                                .title("\uD83C\uDF41WithU API")
-                                .version("1.0")
-                                .description("API Test용 ID : "));
+                .info(info)
+                .addServersItem(new Server().url(serverUrl));
     }
 
-    @Bean
-    public GroupedOpenApi customGroupedOpenApi() {
-        return GroupedOpenApi.builder().group("api").pathsToMatch("/**").build();
-    }
+    Info info = new Info().title("\uD83C\uDF41WithU API").version("0.0.1").description(
+            "<h3>WithU Backend APIS</h3>");
 
 }
