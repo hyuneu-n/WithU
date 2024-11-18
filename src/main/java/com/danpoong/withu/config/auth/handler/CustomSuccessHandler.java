01d77f199b5b6ef8db2feda4281227e052d3b5c9
JJERY <hyuneun@skuniv.ac.kr>
2024-11-18 14:52:41 +0900

    🐛 Fix: 카카오 재로그인 오류 해결!

--- src/main/java/com/danpoong/withu/config/auth/handler/CustomSuccessHandler.java
+++ src/main/java/com/danpoong/withu/config/auth/handler/CustomSuccessHandler.java
@@ -1,5 +1,6 @@
 package com.danpoong.withu.config.auth.handler;
 
+import com.danpoong.withu.config.auth.dto.CustomOAuth2User;
 import com.danpoong.withu.config.auth.jwt.JwtUtil;
 import com.danpoong.withu.user.repository.UserRepository;
 import jakarta.servlet.http.Cookie;
@@ -9,6 +10,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
 
@@ -26,21 +28,11 @@ public class CustomSuccessHandler extends SimpleUrlAuthenticationSuccessHandler 
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException {
         // OAuth2User에서 이메일과 닉네임 추출
-        DefaultOAuth2User oAuth2User = (DefaultOAuth2User) authentication.getPrincipal();
+        OAuth2User oAuth2User = (OAuth2User) authentication.getPrincipal();
         Map<String, Object> attributes = oAuth2User.getAttributes();
 
-        String email = null;
-        String nickname = null;
-
-        if (attributes.containsKey("kakao_account")) {
-            Map<String, Object> kakaoAccount = (Map<String, Object>) attributes.get("kakao_account");
-            email = (String) kakaoAccount.get("email");
-        }
-
-        if (attributes.containsKey("properties")) {
-            Map<String, Object> properties = (Map<String, Object>) attributes.get("properties");
-            nickname = (String) properties.get("nickname");
-        }
+        String email = (String) attributes.get("email");
+        String nickname = (String) attributes.get("nickname");
 
         log.info("Authentication successful. Extracted email: {}", email);
         log.info("Authentication successful. Extracted nickname: {}", nickname);
@@ -63,10 +55,10 @@ public class CustomSuccessHandler extends SimpleUrlAuthenticationSuccessHandler 
             // 응답 본문에 리다이렉트 URL과 JWT 반환
             response.setContentType("application/json");
             response.setCharacterEncoding("UTF-8");
-            response.getWriter().write("{ \"redirectUrl\": \"http://localhost:3000/home\", \"accessToken\": \"" + accessToken + "\" }");
+            response.getWriter().write("{ \"redirectUrl\": \"http://localhost:8080/api/users/home\", \"accessToken\": \"" + accessToken + "\" }");
 
             // 리다이렉트 설정
-            getRedirectStrategy().sendRedirect(request, response, "http://localhost:8080/home");
+            getRedirectStrategy().sendRedirect(request, response, "http://localhost:8080/api/users/home");
         } else {
             log.error("Email is null. Cannot generate JWT.");
             response.sendError(HttpServletResponse.SC_BAD_REQUEST, "Invalid authentication data");
