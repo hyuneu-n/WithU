4543f703dc19d4a2203d6cd5f6910b2557dcb129
JJERY <hyuneun@skuniv.ac.kr>
2024-11-18 21:12:09 +0900

    ♻️ Refactor: 생일 string에서 localdate로 변경

--- src/main/java/com/danpoong/withu/user/dto/UserRegisterRequest.java
+++ src/main/java/com/danpoong/withu/user/dto/UserRegisterRequest.java
@@ -2,9 +2,11 @@ package com.danpoong.withu.user.dto;
 
 import lombok.Data;
 
+import java.time.LocalDate;
+
 @Data
 public class UserRegisterRequest {
     private String nickname;
     private String profileImage;
-    private String birthday;
+    private LocalDate birthday;
 }
--- src/main/java/com/danpoong/withu/user/dto/UserResponse.java
+++ src/main/java/com/danpoong/withu/user/dto/UserResponse.java
@@ -4,6 +4,8 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDate;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
@@ -11,6 +13,7 @@ public class UserResponse {
     private String email;
     private String nickname;
     private String profileImage;
-    private String birthday;
+    private LocalDate birthday;
     private String role;
+    private Long familyId;
 }
